a4992dca19fa8d828324fe6545abf0d6768de85e
David seco <deivid.develop@gmail.com>
2023-03-14 18:38:47 +0100

    Update config jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     testEnvironment: 'jest-environment-jsdom',
     setupFiles: ['./jest.setup.js'],
+    setupFilesAfterEnv: [
+      "@testing-library/jest-dom/extend-expect"
+    ],
     moduleNameMapper: {
         "^.+\\.svg$": "jest-svg-transformer",
 	"\\.(css|less|scss)$": "identity-obj-proxy",
