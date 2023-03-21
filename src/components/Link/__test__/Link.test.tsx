bc04ecbf91b8ba896ff2c4b0f153ab8f6bbc9a4b
David seco <deivid.develop@gmail.com>
2023-03-21 09:32:46 +0100

    Feat: test link component

--- src/components/Link/__test__/Link.test.tsx
+++ src/components/Link/__test__/Link.test.tsx
@@ -2,6 +2,6 @@ import { render, screen } from '@testing-library/react';
 import Link from '../Link';
 
 test('Renders main page correctly', async () => {
-  render(<Link />);
-  expect(true).toBeTruthy();
+  const { container } =render(<Link />);
+  expect(container).toBeInTheDocument()
 });
