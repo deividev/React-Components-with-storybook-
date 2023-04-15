a8b1b79db5efcc6ff43802bb53d310ac3c4c7755
David seco <deivid.develop@gmail.com>
2023-04-15 16:27:10 +0200

    fix: testin icon component

--- src/components/Icon/__test__/Icon.test.tsx
+++ src/components/Icon/__test__/Icon.test.tsx
@@ -1,7 +1,57 @@
-import { render } from '@testing-library/react';
-import Icon from '../Icon';
+import { render } from "@testing-library/react";
+import Icon from "../Icon";
 
-test('Render icon component correctly', async () => {
-  const { container } = render(<Icon icon={'cart_shopping'} />);
+test("Render icon component correctly", () => {
+  const { container } = render(<Icon icon={"cart_shopping"} />);
   expect(container).toBeInTheDocument();
+});
+
+test("Render icon component to snapshot", () => {
+  const { container } = render(<Icon icon={"cart_shopping"} />);
+  expect(container).toMatchSnapshot();
+});
+
+it("should render with the correct styles to add props.color", () => {
+  const nameIcon = "cart_shopping";
+  const { getByTestId } = render(<Icon color="red" icon={nameIcon} />);
+  const svgIcon = getByTestId(nameIcon);
+  const paths = svgIcon.querySelectorAll("path");
+  const secondPath = paths[1];
+  expect(secondPath.getAttribute("fill")).toBe("red");
+});
+
+it("should render with the correct styles to add props.className", () => {
+  const nameIcon = "cart_shopping";
+  const className = { fill: "red" };
+  const { getByTestId } = render(
+    <Icon className={className} icon={nameIcon} />
+  );
+  const svgIcon = getByTestId(nameIcon);
+  const paths = svgIcon.querySelectorAll("path");
+  const secondPath = paths[1];
+  expect(secondPath.getAttribute("fill")).toBe("red");
+});
+
+it("should render with the correct styles to add props.className not property fill", () => {
+  const nameIcon = "cart_shopping";
+  const className = {};
+  const { getByTestId } = render(
+    <Icon color="red" className={className} icon={nameIcon} />
+  );
+  const svgIcon = getByTestId(nameIcon);
+  const paths = svgIcon.querySelectorAll("path");
+  const secondPath = paths[1];
+  expect(secondPath.getAttribute("fill")).toBe("red");
+});
+
+it("should render with the correct styles to default color", () => {
+  const nameIcon = "cart_shopping";
+  const className = {};
+  const { getByTestId } = render(
+    <Icon className={className} icon={nameIcon} />
+  );
+  const svgIcon = getByTestId(nameIcon);
+  const paths = svgIcon.querySelectorAll("path");
+  const secondPath = paths[1];
+  expect(secondPath.getAttribute("fill")).toBe("black");
 });
