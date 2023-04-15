178ad9b2b4d1e58587a7d5439a4db83dce1c3c69
David seco <deivid.develop@gmail.com>
2023-04-15 16:14:28 +0200

    fix: change styles default

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,43 +1,48 @@
 import { ICONS } from "../../constants/icons";
 
 interface IconProps {
-    /**
-      * Name Icon to display
-    */
-    icon: string; 
-    /**
-      * Icon size to display
-    */
-    size?: number;
-    /**
-      * Color to icon, black deafult color 
-    */
-    color?: string;
-    /**
-      * Event click
-    */
-    onClick?: () => void;
-    /**
-     * Class to change styles , focus, hover, fill etc... 
-    */
-    className?: any;
-  }
+  /**
+   * Name Icon to display
+   */
+  icon: string;
+  /**
+   * Icon size to display
+   */
+  size?: number;
+  /**
+   * Color exadecimal to icon, black deafult
+   */
+  color?: string;
+  /**
+   * Event click
+   */
+  onClick?: () => void;
+  /**
+   * Class to change styles , focus, hover, fill predominates over the color including default  etc...
+   */
+  className?: any;
+}
 
+export const Icon = ({ icon, ...props }: IconProps) => {
+  const { color } = props;
+  const iconColor = color ? color : "black";
 
-export const Icon = ({
-    icon,
-    ...props
-  }: IconProps) => {
-  
   return (
-    <svg className={props.className} onClick={props.onClick}
-      width={`${props.size ? props.size : '24'}px`}
-      height={`${props.size ? props.size : '24'}px`}
+    <svg
+      data-testid={icon}
+      className={props.className}
+      onClick={props.onClick}
+      width={`${props.size ? props.size : "24"}px`}
+      height={`${props.size ? props.size : "24"}px`}
       viewBox="0 0 24 24"
     >
-	    <path fill="none" d="M0 0h24v24H0z"/>
+      <path fill="none" d="M0 0h24v24H0z" />
       <path
-		    fill={props.className ? props.className.fill : props.color}
+        fill={
+          props.className && props.className.fill
+            ? props.className.fill
+            : iconColor
+        }
         d={ICONS[icon]}
       ></path>
     </svg>
